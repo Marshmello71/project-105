3aa0ec965e619aa7e0fe637f6bc5f4859d1cc0ce
Marshmello71 <85738840+Marshmello71@users.noreply.github.com>
2021-10-06 17:51:58 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+Webcam.set({
+    width:350,
+    height:350,
+    image_format : 'png',
+    png_quality : 100
+})
+
+camera = docuument.getElementById("camera");
+
+Webam.attach('#camera');
+
+function take_snapshot()
+{
+    Webcam.snap (function (data_uri){    
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'">'
+    });
+}
+
+console.log('ml5 version:', ml5.version);
+
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/8vhUUTc1S/',modelLoaded);
+
+function take_snapshot()
+{
+    img = document.getElementById('captured_image');
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error, results)
+{
+    if (error){
+        console.error(error);
+    }
+    else 
+    {
+        console.log(results);
+        document.getElementById("result_object_name").innerHTML = results[0].label;
+        document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.tofixed(3);
+    }       
+}
